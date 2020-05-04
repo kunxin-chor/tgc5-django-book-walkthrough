cc157a252a7ef7ba6920edad7b010889fbc04161
kunxin-chor <chorkunxin@yahoo.com>
2020-05-04 07:13:37 +0000

    Added static files

--- books/views.py
+++ books/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    # return HttpResponse("Hello")
+    return render(request, 'books/index.template.html')
