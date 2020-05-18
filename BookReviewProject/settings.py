533dd835451e7d37ba12f3311beb8ff9dfa23d51
kunxin-chor <chorkunxin@yahoo.com>
2020-05-18 08:13:40 +0000

    Email demo

--- BookReviewProject/settings.py
+++ BookReviewProject/settings.py
@@ -102,7 +102,20 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/books'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# if 'DEVELOPMENT' in os.environ:
+#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#     DEFAULT_FROM_EMAIL = "admin@gemail.com"
+# else:
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+
+print(EMAIL_HOST_USER)
+print(EMAIL_HOST_PASSWORD)
 
 WSGI_APPLICATION = 'BookReviewProject.wsgi.application'
 
